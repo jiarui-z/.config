0cd6dd22910378edb2d8960785dbd656ccbf54ad
Jiarui Zhang <j879zhan@uwaterloo.ca>
2022-01-11 20:40:32 -0500

    updated the cursor shape
    temp

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,5 +1,6 @@
 # Mark the name as a function
 # some other useful info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
+# https://unix.stackexchange.com/questions/33255/how-to-define-and-load-your-own-shell-function-in-zsh
 autoload edit-command-line; zle -N edit-command-line
 bindkey -v
 
@@ -12,29 +13,25 @@ bindkey '^e' edit-command-line
 # bindkey -M vicmd "L" vi-end-of-line
 # bindkey -M vicmd "i" up-line-or-history
 # bindkey -M vicmd "k" down-line-or-history
-bindkey -v '^?' backward-delete-char
 
 # Change cursor shape for different vi modes.
 function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
+  if [[ $KEYMAP == vicmd ]] ||
      [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
+    echo -ne '\e[2 q'
+  elif [[ $KEYMAP == main ]] ||
+       [[ $KEYMAP == viins ]] ||
+       [[ $KEYMAP = '' ]] ||
        [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
+    echo -ne '\e[6 q'
   fi
   zle reset-prompt
 }
 zle -N zle-keymap-select
 
-# Use beam shape cursor on startup
-echo -ne '\e[5 q'
-
 # Use beam shape cursor for each new prompt
 _fix_cursor() {
-   echo -ne '\e[5 q'
+  echo -ne '\e[6 q'
 }
 precmd_functions+=(_fix_cursor)
 
