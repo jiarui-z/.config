decbcf92d41c33a14c7c0c01477707e42d0f4a6f
Jiarui Zhang <41531801+jiarui-z@users.noreply.github.com>
2024-02-10 17:10:36 -0500

    Update env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,12 +1,2 @@
-# export XDG_CONFIG_HOME=$HOME/.config
-# export LOCALBIN=$XDG_CONFIG_HOME/bin
-export PATH=$PATH:/usr/local/bin
-export PATH=$PATH:$HOME/.npm-global/bin
-export PATH=$PATH:$HOME/go/bin
-export GOPATH=$HOME/go
-export TERM=xterm-256color
-export TERM_ITALICS=true
+export XDG_CONFIG_HOME=$HOME/.config
 export EDITOR=nvim
-export RANGER_LOAD_DEFAULT_RC=false
-export ZIM_HOME=$HOME/.zim
-export BAT_THEME="Nord"
